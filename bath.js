e1e123fb92e9549be7af611e97958c04f9521c05
Jake Parrish <9jparrish@gmail.com>
2018-01-15 11:05:10 -0500

    Cool Stuff

---
+++ bath.js
@@ -0,0 +1,9 @@
+var a = prompt("Give me a noun ");
+var b = prompt("Give me a noun ");
+var c = prompt("Give me a person ");
+var d = prompt("Give me an adjective ");
+
+var x = "The other day I ran aroud the " + a + ", I stopped after I saw the " + b + ". Then all of a sudden we met " + c + ". " + d + " person but it was still cool!";
+
+
+document.write(x);
